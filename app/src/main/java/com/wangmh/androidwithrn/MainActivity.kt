12cd98f2c58693d0ebb6e24c11a3bd7dc1f447c2
menghua.wang <menghua.wang@17zuoye.com>
2018-10-18 23:26:49 +0800

    commit log :redundant overriding method.

--- app/src/main/java/com/wangmh/androidwithrn/MainActivity.kt
+++ app/src/main/java/com/wangmh/androidwithrn/MainActivity.kt
@@ -7,6 +7,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        /**
+         *  在kotlin中如果在重写Activity的生命周期没有任何信息
+         *  会有这个提示RedUndant overriding method(在kotlin  override function上
+         *
+         *  Kotlin Version：1.2.61
+         */
+
     }
 }
