b4b2a319884a0956fc49f47f6b861549e8af9991
menghua.wang <menghua.wang@17zuoye.com>
2018-10-23 23:32:53 +0800

    commit.

--- app/src/main/java/com/wangmh/androidwithrn/MainActivity.kt
+++ app/src/main/java/com/wangmh/androidwithrn/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var TAG :String = MainActivity::class.simpleName!!
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         /**
          *  在kotlin中如果在重写Activity的生命周期没有任何信息
@@ -24,5 +28,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
